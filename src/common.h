890e9510bb5cbac2e6dfe273d9ca98c345e257c8
Teemu Toivola <git@humdi.net>
2014-02-25 22:23:57 +0200

    this is version 1.12 beta already

--- src/common.h
+++ src/common.h
@@ -129,7 +129,7 @@ and most can be changed later from the config file.
 #define DBVERSION 3
 
 /* version string */
-#define VNSTATVERSION "1.11"
+#define VNSTATVERSION "1.12 beta"
 
 /* xml format version */
 /* 1 = 1.7- */
