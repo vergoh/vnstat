99c7399f383494db4bcbf93bba4368ce7406865a
Teemu Toivola <git@humdi.net>
2015-01-18 20:38:23 +0200

    final preparations for version 1.13 release

--- src/common.h
+++ src/common.h
@@ -151,7 +151,7 @@ and most can be changed later from the config file.
 #define DBVERSION 3
 
 /* version string */
-#define VNSTATVERSION "1.13 beta"
+#define VNSTATVERSION "1.13"
 
 /* xml format version */
 /* 1 = 1.7- */
