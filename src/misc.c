e9568426795048edb01bfe703bfc68650cb8ec00
Teemu Toivola <git@humdi.net>
2015-07-09 20:41:41 +0300

    fix padding for Asian UTF-8 date strings

--- src/misc.c
+++ src/misc.c
@@ -1,5 +1,7 @@
+#define _XOPEN_SOURCE 600
 #include "common.h"
 #include "misc.h"
+#include <wchar.h>
 
 int kerneltest(void)
 {
@@ -395,10 +397,14 @@ char *getratestring(float rate, int len, int declen, int unit)
 
 int getpadding(int len, char *str)
 {
+	wchar_t wbuffer[64];
 	if (!cfg.utflocale) {
 		return len;
 	}
-	return len + ((int)strlen(str) - (int)mbstowcs(NULL, str, 0));
+	if ((int)mbstowcs(wbuffer, str, 64) < 0) {
+		return len;
+	}
+	return len + ((int)strlen(str) - wcswidth(wbuffer, 64));
 }
 
 void cursortocolumn(int column)
