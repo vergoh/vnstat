4d72660d7d93aa030dc4b816cbf8da8a2596b709
Teemu Toivola <git@humdi.net>
2018-01-07 19:39:52 +0200

    2017 -> 2018

--- src/vnstati.c
+++ src/vnstati.c
@@ -1,5 +1,5 @@
 /*
-vnStat image output - Copyright (c) 2007-2017 Teemu Toivola <tst@iki.fi>
+vnStat image output - Copyright (c) 2007-2018 Teemu Toivola <tst@iki.fi>
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
